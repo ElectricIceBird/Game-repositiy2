8d1718f135a9781ac39a3e6c950d96a0f4d383c2
ElectricIceBird <electricicebird23@gmail.com>
2022-05-07 23:12:06 +0500

    Commit 2 Endless runner ad

--- Endlessrunner3D/Assets/ExternalDependencyManager/admanager.cs
+++ Endlessrunner3D/Assets/ExternalDependencyManager/admanager.cs
@@ -7,11 +7,30 @@ using System;
 public class admanager : MonoBehaviour
 {
     private BannerView ad;
+    private InterstitialAd insterstitial;
+
+    public static admanager instance;
     // Start is called before the first frame update
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+   public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        
+        }else{Destroy(gameObject);
+        } 
+           }
     void Start()
     {
      MobileAds.Initialize(InitializationStatus => { });
         this.requestAd();
+    }
+    private AdRequest CreateAdRequest()
+    {
+        return new AdRequest.Builder().Build();
     }
     private void requestAd()
     {
@@ -19,7 +38,30 @@ public class admanager : MonoBehaviour
         this.ad = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
         AdRequest re = new AdRequest.Builder().Build();
         this.ad.LoadAd(re);
+        this.ad.LoadAd(this.CreateAdRequest());
+
     }
+    public void RequestAdinter()
+    {
+           string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+        if(this.insterstitial !=null )
+        {
+            this.insterstitial.Destroy();
+        }
+        this.insterstitial = new InterstitialAd(adUnitId);
+        this.insterstitial.LoadAd(this.CreateAdRequest());
+
+       }
+       public void showInter()
+       {
+           if(this.insterstitial.IsLoaded())
+           {
+               insterstitial.Show();
+           }else
+           {
+               return;
+           }
+       }
     // Update is called once per frame
     void Update()
     {
--- Endlessrunner3D/Assets/Scripts/DisplayManager.cs
+++ Endlessrunner3D/Assets/Scripts/DisplayManager.cs
@@ -7,31 +7,44 @@ public class DisplayManager : MonoBehaviour
 {
     public static bool gameover;
     public GameObject goPanel;
+
+    int adInt;
+
+
     public static bool isStarted;
     public GameObject startText,Score_text,Distance_text;
     public ScoreScript SC;
     // Start is called before the first frame update
     void Start()
     {
+
+            admanager.instance.RequestAdinter();
+
         SC.enabled = false;
         Time.timeScale = 1;
         gameover = false;
         isStarted = false;
-      
+        adInt = Mathf.RoundToInt(Random.Range(0f,3f));
+
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-       
-        if (gameover)
+
+       if (gameover)
         {
             Time.timeScale = 0;
             goPanel.SetActive(true);
 
             Destroy(Score_text);
             Destroy(Distance_text);
+            if(adInt==0)
+            {
+            admanager.instance.showInter();
+            }
 
         }
         if (SwipeManager.tap)
